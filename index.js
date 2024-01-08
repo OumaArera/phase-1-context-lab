3e5c9c71c9a0bcecee29ccf6752c2f4300ddbccb
OumaArera <johnouma999@gmail.com>
2024-01-08 11:43:58 +0300

    feat: done

--- index.js
+++ index.js
@@ -1,5 +1,68 @@
 /* Your Code Here */
 
+const createEmployeeRecord = employeeArray => {
+    return {
+      firstName: employeeArray[0],
+      familyName: employeeArray[1],
+      title: employeeArray[2],
+      payPerHour: employeeArray[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+const createEmployeeRecords = arrays => arrays.map(createEmployeeRecord);
+
+const createTimeInEvent = (dateStamp) => {
+  let timeInEvents = [];
+  const [date, time] = dateStamp.split(' ');
+  const [year, month, day] = date.split('-');
+  // const [hour, minute] = time.split('');
+
+  const timeInEvent = {
+    type: "TimeIn",
+    hour: `${time}`,
+    date: `${year}-${month}-${day}`,
+  };
+  timeInEvents.push(timeInEvent);
+  return employee;
+}
+
+// const createTimeOutEvent = (employeeRecord, dateStamp) => {
+//     const [date, time] = dateStamp.split(' ');
+//     const [year, month, day] = date.split('-');
+//     const [hour, minute] = time.split(':');
+  
+//     const timeOutEvent = {
+//       type: "TimeOut",
+//       hour: parseInt(hour),
+//       date: `${year}-${month}-${day}`,
+//     };
+//     employeeRecord.timeOutEvents.push(timeOutEvent)
+//     return employeeRecord;
+// }
+
+// const hoursWorkedOnDate = (employeeRecord, date) => {
+//     const timeInEvent = employeeRecord.timeInEvents.find(event => event.date === date);
+//     const timeOutEvent = employeeRecord.timeOutEvents.find(event => event.date === date);
+  
+//     if (!timeInEvent || !timeOutEvent) {
+//       return 0;
+//     }
+  
+//     const timeInHour = timeInEvent.hour;
+//     const timeOutHour = timeOutEvent.hour;
+  
+//     return ((timeOutHour - timeInHour)/100);
+//   }
+
+// const wagesEarnedOnDate = (employeeRecord, date) => {
+// const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+// const payRate = employeeRecord.payPerHour;
+
+// return hoursWorked * payRate;
+// }
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -9,7 +72,7 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
+const allWagesFor = () => {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
